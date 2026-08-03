5b7940a89102ad1cd69d85f1ba6e28008912fcad
Charmila Chokkarapu <charmilachokkarapu2@gmail.com>
2026-08-03 15:17:44 +0530

    Fix cartSlice filename

--- src/components/ChainCard.jsx
+++ src/components/ChainCard.jsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { addFavorite } from "../features/favoriteSlice";
-import { addToCart } from "../features/cartSlice";
+import { addToCart } from "../features/Cartslice";
 
 function ChainCard({ chain }) {
   const dispatch = useDispatch();
